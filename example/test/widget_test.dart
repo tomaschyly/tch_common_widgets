c934a77628f56e3784a4225df2b5b0a055b13d24
tomaschyly <chyly@tomas-chyly.com>
2026-04-06 12:29:03 +0200

    FCCW-100/Code modernization

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,27 +1,12 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tch_common_widgets_example/main.dart';
 
+/// Run widget tests for the example app.
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('shows app bar title', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
 
-    // Verify that platform version is retrieved.
-    expect(
-      find.byWidgetPredicate(
-        (Widget widget) => widget is Text &&
-                           widget.data!.startsWith('Running on:'),
-      ),
-      findsOneWidget,
-    );
+    expect(find.text('Plugin example app'), findsOneWidget);
   });
 }
